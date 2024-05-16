7fa6adf097900de70de17a0a98cd86415394d085
Deepak Sharma <46669654+deepakprogrammer@users.noreply.github.com>
2024-05-16 08:22:43 +0530

    setup workspace

---
+++ src/main/java/com/coderhack/leaderboard/CoderHackLeaderboardApplication.java
@@ -0,0 +1,13 @@
+package com.coderhack.leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderHackLeaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderHackLeaderboardApplication.class, args);
+	}
+
+}
