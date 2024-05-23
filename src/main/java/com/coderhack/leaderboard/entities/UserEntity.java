67544fb016c173d205e1ad119c3b6c58ea88c942
Deepak Sharma <46669654+deepakprogrammer@users.noreply.github.com>
2024-05-23 22:20:41 +0530

    In UserEntity correction of @Document(collection = users) insted of @Document(collaction = 'users')

--- src/main/java/com/coderhack/leaderboard/entities/UserEntity.java
+++ src/main/java/com/coderhack/leaderboard/entities/UserEntity.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Data
-@Document(collation = "users")
+@Document(collection = "users")
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserEntity {
